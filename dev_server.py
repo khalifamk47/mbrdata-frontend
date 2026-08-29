551660249399f92755f214e0612fd93b78bc8f13
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-08-29 11:09:37 +0100

    Add no-cache local frontend server

---
+++ dev_server.py
@@ -0,0 +1,12 @@
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+
+
+class NoCacheHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
+
+ThreadingHTTPServer(("127.0.0.1", 4173), NoCacheHandler).serve_forever()
